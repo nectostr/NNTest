005a05c230e604783160f68f50d7d1dce111aea8
nectostr <bastinda96@gmail.com>
2017-06-09 12:58:02 +0300

    Add files via upload

---
+++ NeuralNetwork.cs
@@ -0,0 +1,89 @@
+﻿using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
+using System;
+
+namespace ConsoleApp2
+{
+    public class NeuralNetwork
+    {
+        Random rnd = new Random();
+        const int amount = 2;
+
+        int[,] gInput = new int[4,2] { {0,0},{0,1 },{1,0 },{1,1 } };
+        int[] answers = new int[4] {0,1,1,0 };
+        
+        
+
+        Neuron[] hiddenNeurons = new Neuron[amount];
+        Neuron[] inputNeurons = new InputNeuron[amount];
+        Neuron outputNeuron;
+
+        void init()
+        {
+            for (int i = 0; i < inputNeurons.Length; i++)
+            {
+                for (int j = 0; j < inputNeurons[i].synapths.Length; j++)
+                    inputNeurons[i].synapths[j] = 1;
+            }
+            for (int i = 0; i < hiddenNeurons.Length; i++)
+            {
+                for (int j = 0; j < hiddenNeurons[i].synapths.Length; j++)
+                    hiddenNeurons[i].synapths[j] = rnd.Next();
+            }
+            for (int j = 0; j < outputNeuron.synapths.Length; j++)
+                outputNeuron.synapths[j] = rnd.Next();            
+        }
+        // проброс значений через сетку, оптимизировать
+        double thnk(int a)
+        {
+            for (int i = 0; i < inputNeurons.Length; i++)
+            {
+                for (int j = 0; j < 2; j ++)
+                    inputNeurons[i].inputs[j] = gInput[a,i];
+            }
+            for (int i = 0; i < inputNeurons.Length; i++)
+            {
+                inputNeurons[i].thnk();
+            }
+            for (int i = 0; i < hiddenNeurons.Length; i++)
+            {
+                for (int j = 0; j < hiddenNeurons[i].inputs.Length; j++)
+                   hiddenNeurons[i].inputs[j] = inputNeurons[i].output;
+            }
+            for (int i = 0; i < hiddenNeurons.Length; i++)
+            {
+                hiddenNeurons[i].thnk();
+            }
+            for (int i = 0; i < outputNeuron.inputs.Length; i++)
+                outputNeuron.inputs[i] = hiddenNeurons[i].output;
+            return outputNeuron.output;
+        }
+        double gError;
+        double[] errors = new double[2];
+        // обучим сетку
+        public int teachMe()
+        {
+            for (int p = 0; p < 4; p++)
+            {
+                thnk(p);
+                double lErr = answers[p] - outputNeuron.output;
+                gError += Math.Abs(lErr);
+                outputNeuron.makeErrCh(lErr);
+                lErr = 0;
+                for (int i = 0; i < 1; i++) // стремная константа количества нейронов в слое, который у меня пока даже не массив
+                {
+
+                }
+             
+
+
+
+            }
+            return 0;
+        }
+
+
+
+    }
+}
---
+++ Neuron.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace ConsoleApp2
+{
+    public class Neuron
+    {
+        const int amInputs = 2;
+
+        public double[] inputs = new double[amInputs];
+        public double[] synapths= new double[amInputs];
+        public double output;
+        //public double error;
+        private double ownWeight = 0.3;
+        
+
+        public void thnk()
+        {
+            //функция суммирования
+            output = 0;
+            for(int i = 0; i < inputs.Length; i++)
+            {
+                output += inputs[i] * synapths[i];
+            }
+            output += 1 * ownWeight;
+            /// функция активации
+            output = 1 / (1 + Math.Pow(Math.E, output));
+
+        }
+
+        public void makeErrCh(double error)
+        {
+            for (int i = 0; i < synapths.Length; i++)
+            {
+                synapths[i] = synapths[i]
+                    * (-Math.Pow(Math.E, synapths[i]) / (Math.Pow((Math.Pow(Math.E, synapths[i]) + 1), 2)))//производная активации
+                    * error  //ошибка
+                    * 0.01; // скорость обучения
+
+            }
+        }
+    }
+}
